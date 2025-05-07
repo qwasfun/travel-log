5e127782b0b7e5b5c1777b3091d54413eee90225
qwas <git@qwas.fun>
2025-05-07 11:06:36 +0800

    add tailwindcss, daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
   modules: ["@nuxt/eslint"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
